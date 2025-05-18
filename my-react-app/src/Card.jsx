4d1272ad30bced455de326abb08959c00d71b7f7
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-05-18 22:49:55 +0800

    cx

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,9 +1,12 @@
+import profilePic from './assets/pic.jpg'
 
 function Card (){
 
     return(
         <div className="card">
-            <img></img>
+            <img alt="profile" src={profilePic}></img>
+            <h1>DevChad</h1>
+            <p>A 2nd Year BSIT Student</p>
         </div>
 
     );
