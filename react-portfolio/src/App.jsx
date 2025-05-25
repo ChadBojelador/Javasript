4a2d4138880f3246a62164f92508858f62f88e23
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-05-25 12:31:38 +0800

    header

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,46 +1,52 @@
-import homeIcon from './assets/home.svg';
-import folderIcon from './assets/folder.svg'
-import certificateIcon from './assets/certificates.svg'
-import toolsIcon from './assets/tools.svg'
-import aboutIcon from './assets/about.svg'
+import home from './assets/homeIcon.svg';
+import folderIcon from './assets/folder.svg';
+import certificateIcon from './assets/certificates.svg';
+import toolsIcon from './assets/tools.svg';
+import aboutIcon from './assets/about.svg';
+import picture from './assets/picture.svg'
+const navItems = [
+  { icon: home, alt: "Home", class: "home" },
+  { icon: folderIcon, alt: "Projects", class: "folder" },
+  { icon: certificateIcon, alt: "Certificates", class: "certificate" },
+  { icon: toolsIcon, alt: "Tools", class: "tools" },
+  { icon: aboutIcon, alt: "About", class: "about" },
+];
 
 function App() {
-  return(
-  <div className='header-container'>
-    
-        <header>
-          <nav>
-            <ul className='list-container'>
-              <li className='list'>
-                   <img className="icon-Home" src={homeIcon} alt="Home icon" />
+  return (
+    <div className="header-container">
+      <header>
+        <nav>
+          <ul className="list-container">
+            {navItems.map((item, index) => (
+              <li className="list" key={index}>
+                <img
+                  className={`icon icon-${item.class}`}
+                  src={item.icon}
+                  alt={item.alt}
+                />
               </li>
-               <li className='list'>
-                   <img className="icon-Home" src={folderIcon} alt="Home icon" />
-              </li>
-               <li className='list'>
-                   <img className="icon-Home" src={certificateIcon} alt="Home icon" />
-              </li>
-               <li className='list'>
-                   <img className="icon-Home" src={toolsIcon} alt="Home icon" />
-              </li>
-               <li className='list'>
-                   <img className="icon-Home" src={aboutIcon} alt="Home icon" />
-              </li>
-            </ul>
-          </nav>
-        </header>
-    <div className='header-container'>
-        <aside>
-          <div className='picture-block'>
+            ))}
+          </ul>
+        </nav>
+      </header>
 
-          </div>
+      <div className="header-container2">
+        <aside>
+          <img className="picture"src={picture}/>
+          <h1 className='name'>Chad Bojelador</h1>
         </aside>
         <section>
-
+          <div className='title-container'>
+            <h1 className='Title'>SOFTWARE</h1>
+            <h1 className='Title' id='title1'>DEVELOPER</h1>
+          </div>
         </section>
+      </div>
     </div>
-  </div>
+
+   
   );
 }
 
-export default App
+export default App;
