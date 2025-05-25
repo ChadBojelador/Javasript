d8aa8eb68f9af11359ae6a0d4ee9df71753fee79
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-05-25 23:17:13 +0800

    xcx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,9 +1,11 @@
+import { useState, useEffect, useRef } from 'react';
 import home from './assets/homeIcon.svg';
 import folderIcon from './assets/folder.svg';
 import certificateIcon from './assets/certificates.svg';
 import toolsIcon from './assets/tools.svg';
 import aboutIcon from './assets/about.svg';
-import picture from './assets/picture.svg'
+import picture from './assets/picture.svg';
+import CustomCursor from './CustomCursor';
 const navItems = [
   { icon: home, alt: "Home", class: "home" },
   { icon: folderIcon, alt: "Projects", class: "folder" },
@@ -13,7 +15,15 @@ const navItems = [
 ];
 
 function App() {
+    useEffect(() => {
+    document.body.style.cursor = 'none';
+    return () => document.body.style.cursor = 'default';
+  }, []);
+
+
   return (
+    <div className="App">
+      <CustomCursor />
     <div className="header-container">
       <header>
         <nav>
@@ -33,27 +43,49 @@ function App() {
 
       <div className="header-container2">
         <aside>
-          <img className="picture"src={picture}/>
-            <div className='description-pic'>  
+          <img className="picture" src={picture} alt="Profile" />
+          <div className='description-pic'>  
             <h1 className='name'>Chad Bojelador</h1>
             <p>A Software Developer as well as a Bachelor of Science in Information Technology.</p>
-            </div>
-
-        
+          </div>
         </aside>
+
         <section>
           <div className='title-container'>
             <h1 className='Title'>SOFTWARE</h1>
             <h1 className='Title' id='title1'>DEVELOPER</h1>
-            <p>Passionate about building impactful software that blends functionality </p>
-            <p>with exceptional user experience. I specialize in crafting digital </p>
-              <p>products that people love to use.</p>
+            <div className='p-container'>
+              <p>Focused on building software that drives impact</p>
+              <p>with intuitive design and seamless functionality.</p>
+            </div>
+          </div>
+
+          {/* Add content-sections container */}
+          <div className="content-sections">
+            <div id="techstack" className="content-part">
+              <h2>Tech Stack</h2>
+            </div>
+            
+            <div id="projects" className="content-part">
+              <h2>Projects</h2>
+            </div>
+
+            <div id="experience" className="content-part">
+              <h2>Experience</h2>
+            </div>
+
+            <div id="certificates-seminars" className="content-part">
+              <h2>Certificates & Seminars</h2>
+            </div>
+
+            <div id="contacts" className="content-part">
+              <h2>Contacts</h2>
+            </div>
           </div>
         </section>
       </div>
     </div>
-
-   
+    </div>
   );
 }
 
--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './materials.css'
-import App from './App.jsx'
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
+// main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 
 const router = createBrowserRouter([
-            {path:"/",element:<App/>},]);
-
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
