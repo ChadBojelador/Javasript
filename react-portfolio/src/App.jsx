d5a954f36735d73905f8f3caa251d90075d2225e
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-05-26 12:03:47 +0800

    fixed

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -6,85 +6,62 @@ import toolsIcon from './assets/tools.svg';
 import aboutIcon from './assets/about.svg';
 import picture from './assets/picture.svg';
 import CustomCursor from './CustomCursor';
+
 const navItems = [
-  { icon: home, alt: "Home", class: "home" },
-  { icon: folderIcon, alt: "Projects", class: "folder" },
-  { icon: certificateIcon, alt: "Certificates", class: "certificate" },
-  { icon: toolsIcon, alt: "Tools", class: "tools" },
-  { icon: aboutIcon, alt: "About", class: "about" },
+  { icon: home, alt: "Home", className: "home" },
+  { icon: folderIcon, alt: "Projects", className: "folder" },
+  { icon: certificateIcon, alt: "Certificates", className: "certificate" },
+  { icon: toolsIcon, alt: "Tools", className: "tools" },
+  { icon: aboutIcon, alt: "About", className: "about" },
 ];
 
 function App() {
-    useEffect(() => {
+  useEffect(() => {
     document.body.style.cursor = 'none';
     return () => document.body.style.cursor = 'default';
   }, []);
 
-
   return (
     <div className="App">
       <CustomCursor />
-    <div className="header-container">
-      <header>
-        <nav>
-          <ul className="list-container">
-            {navItems.map((item, index) => (
-              <li className="list" key={index}>
-                <img
-                  className={`icon icon-${item.class}`}
-                  src={item.icon}
-                  alt={item.alt}
-                />
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
+      <div className="header-container">
+        <header>
+          <nav>
+            <ul className="list-container">
+              {navItems.map((item, index) => (
+                <li className="list" key={index}>
+                  <img
+                    className={`icon icon-${item.className}`}
+                    src={item.icon}
+                    alt={item.alt}
+                  />
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </header>
 
-      <div className="header-container2">
-        <aside>
-          <img className="picture" src={picture} alt="Profile" />
-          <div className='description-pic'>  
-            <h1 className='name'>Chad Bojelador</h1>
-            <p>A Software Developer as well as a Bachelor of Science in Information Technology.</p>
-          </div>
-        </aside>
+        <div className="header-container2">
+          <aside>
+            <img className="picture" src={picture} alt="Profile" />
+            <div className="description-pic">  
+              <h1 className="name">Chad Bojelador</h1>
+              <p>A Software Developer as well as a Bachelor of Science in Information Technology.</p>
+            </div>
+          </aside>
 
-        <section>
-          <div className='title-container'>
-            <h1 className='Title'>SOFTWARE</h1>
-            <h1 className='Title' id='title1'>DEVELOPER</h1>
-            <div className='p-container'>
-              <p>Focused on building software that drives impact</p>
-              <p>with intuitive design and seamless functionality.</p>
+          <section>
+            <div className="title-container">
+              <h1 className="Title">SOFTWARE</h1>
+              <h1 className="Title" id="title1">DEVELOPER</h1>
+              <div className="p-container">
+                <p>Focused on building software that drives impact</p>
+                <p>with intuitive design and seamless functionality.</p>
+              </div>
             </div>
-          </div>
-
-          {/* Add content-sections container */}
-          <div className="content-sections">
-            <div id="techstack" className="content-part">
-              <h2>Tech Stack</h2>
-            </div>
-            
-            <div id="projects" className="content-part">
-              <h2>Projects</h2>
-            </div>
-
-            <div id="experience" className="content-part">
-              <h2>Experience</h2>
-            </div>
-
-            <div id="certificates-seminars" className="content-part">
-              <h2>Certificates & Seminars</h2>
-            </div>
-
-            <div id="contacts" className="content-part">
-              <h2>Contacts</h2>
-            </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
